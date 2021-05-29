aa918259c3adc5f138927b6ac4069bee1d172a4d
VivekIly <70602891+VivekIly@users.noreply.github.com>
2021-05-29 15:41:03 -0500

    Completed BrainFactory Todo.

--- BrainFactory.java
+++ BrainFactory.java
@@ -2,29 +2,36 @@ import java.util.ArrayList;
 
 /**
  * This class uses the factory design pattern to provide a list of
- *      objects that implement the Brain interface
+ * objects that implement the Brain interface
  *
  * @author gcschmit
  * @version 24 January 2020
  */
-public class BrainFactory
-{
+public class BrainFactory {
     /**
      * Create a list of references to objects whose classes implement the Brain
-     *      interface
+     * interface
      *
      * @return a list of referenes to objects whose classes implement the Brain
-     *      interface
+     * interface
      */
-    /*public static ArrayList<Brain> createBrains()
-    {
-        *//*
-         * TODO: add instances of SimpleBrain and SmallBrain to the list.
-         * 
-         * If you do the BigBrain extension, also add an instance of BigBrain to
-         *      the list.
-         *//*
-        
-        
-    }*/
+    public static ArrayList<Brain> createBrains() {
+
+         /*TODO:
+        add instances of SimpleBrain and SmallBrain to the list.
+                *
+         *If you do the BigBrain extension, also add an instance of BigBrain to
+         *the list.
+         */
+
+        SimpleBrain simpleBrain = new SimpleBrain();
+        SmallBrain smallBrain = new SmallBrain();
+
+        ArrayList<Brain> brains = new ArrayList<Brain>();
+
+        brains.add(simpleBrain); brains.add(smallBrain);
+
+        return brains;
+
+    }
 }
--- TetrisViewer.java
+++ TetrisViewer.java
@@ -10,47 +10,42 @@ import javax.swing.event.*;
  * @author gcschmit
  * @version 24 January 2020
  */
-public class TetrisViewer
-{
+public class TetrisViewer {
     /**
      * Creates a window, installs the JTetris or JBrainTetris, checks the
-     *      testMode state, install the controls in the EAST.
-    */
-    public static void main(String[] args)
-    {
+     * testMode state, install the controls in the EAST.
+     */
+    public static void main(String[] args) {
         JFrame frame = new JFrame("Tetris 2020");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        JComponent container = (JComponent)frame.getContentPane();
+        JComponent container = (JComponent) frame.getContentPane();
         container.setLayout(new BorderLayout());
-            
+
         final int PIXELS = 16;
-        JTetris tetris = new JTetris((JTetris.WIDTH * PIXELS) + 2,
+        JBrainTetris tetris = new JBrainTetris((JTetris.WIDTH * PIXELS) + 2,
                 (JTetris.HEIGHT + JTetris.TOP_SPACE) * (PIXELS + 2));
-        
-        
+
+
         container.add(tetris, BorderLayout.CENTER);
 
 
-        if (args.length != 0 && args[0].equals("test"))
-        {
+        if (args.length != 0 && args[0].equals("test")) {
             tetris.testMode = true;
         }
-        
+
         Container panel = tetris.createControlPanel();
-        
+
         // Add the quit button last so it's at the bottom
         panel.add(Box.createVerticalStrut(12));
         JButton quit = new JButton("Quit");
         panel.add(quit);
-        quit.addActionListener( new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
+        quit.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
                 System.exit(0);
             }
         });
-        
-        
+
+
         container.add(panel, BorderLayout.EAST);
         frame.pack();
         frame.setVisible(true);
